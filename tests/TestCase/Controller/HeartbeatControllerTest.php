da67f616206bcfa14ca494ddd662c6d28387e171
Livia Scapin <livia.scapin@orca-services.ch>
2018-09-05 15:51:23 +0200

    Added controller test setup (not working)

--- tests/TestCase/Controller/HeartbeatControllerTest.php
+++ tests/TestCase/Controller/HeartbeatControllerTest.php
@@ -3,6 +3,7 @@
 namespace OrcaServices\Heartbeat\Test\TestCase\Controller;
 
 use Cake\Chronos\Chronos;
+use Cake\Collection\Collection;
 use Cake\TestSuite\IntegrationTestCase;
 use OrcaServices\Heartbeat\Heartbeat\Sensor\Status;
 
@@ -15,6 +16,15 @@ class HeartbeatControllerTest extends IntegrationTestCase
 	 * @covers ::index
 	 */
 	public function testIndex() {
-		$this->markTestIncomplete('TODO: Write Test');
+		$this->markTestIncomplete('TODO: Fix this test. Something with routes I guess.');
+		Chronos::setTestNow('2017-03-30 12:45:37');
+		$this->get('/heartbeat');
+		$systemStatus = $this->viewVariable('systemStatus');
+		$this->assertInstanceOf(Status::class, $systemStatus);
+		$sensorStatuses = $this->viewVariable('sensorStatuses');
+		$this->assertInstanceOf(Collection::class, $sensorStatuses);
+		$sensorStatuses->each(function ($sensorStatus) {
+			$this->assertInstanceOf(Status::class, $sensorStatus);
+		});
 	}
 }
