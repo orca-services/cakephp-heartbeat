bd197ac6150ec4cfb84174148dbb550882733532
Livia Scapin <livia.scapin@orca-services.ch>
2018-09-05 11:27:42 +0200

    Added Plugin object

---
+++ src/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+namespace OrcaServices\Heartbeat;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+
+class Plugin extends BasePlugin
+{
+	public function middleware($middleware)
+	{
+		// Add middleware here.
+		return $middleware;
+	}
+
+	public function console($commands)
+	{
+		// Add console commands here.
+		return $commands;
+	}
+
+	public function bootstrap(PluginApplicationInterface $app)
+	{
+		// Add constants, load configuration defaults.
+		// By default will load `config/bootstrap.php` in the plugin.
+		parent::bootstrap($app);
+	}
+
+	public function routes($routes)
+	{
+		// Add routes.
+		// By default will load `config/routes.php` in the plugin.
+		parent::routes($routes);
+	}
+}
