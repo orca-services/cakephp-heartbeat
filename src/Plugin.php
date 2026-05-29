cba77ad99d710792e2c630d9352d72fffbf55390
Neluxx <fabian.arndt96@proton.me>
2026-05-29 15:52:55 +0200

    Remove unnecessary boilerplate code

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,52 +3,11 @@ declare(strict_types=1);
 
 namespace OrcaServices\Heartbeat;
 
-use Cake\Console\CommandCollection;
 use Cake\Core\BasePlugin;
-use Cake\Core\PluginApplicationInterface;
-use Cake\Http\MiddlewareQueue;
-use Cake\Routing\RouteBuilder;
 
 /**
  * CakePHP Heartbeat Plugin
  */
 class Plugin extends BasePlugin
 {
-    /**
-     * @inheritDoc
-     */
-    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
-    {
-        // Add middleware here.
-        return $middlewareQueue;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function console(CommandCollection $commands): CommandCollection
-    {
-        // Add console commands here.
-        return $commands;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function bootstrap(PluginApplicationInterface $app): void
-    {
-        // Add constants, load configuration defaults.
-        // By default will load `config/bootstrap.php` in the plugin.
-        parent::bootstrap($app);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function routes(RouteBuilder $routes): void
-    {
-        // Add routes.
-        // By default will load `config/routes.php` in the plugin.
-        parent::routes($routes);
-    }
 }
