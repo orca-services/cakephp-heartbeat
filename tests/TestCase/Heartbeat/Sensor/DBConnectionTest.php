dd756191a28d806d9ba7608753577702206215f8
Neluxx <fabian.arndt96@proton.me>
2026-06-15 10:13:28 +0200

    Add tests for DBConnection sensor

---
+++ tests/TestCase/Heartbeat/Sensor/DBConnectionTest.php
@@ -0,0 +1,102 @@
+<?php
+declare(strict_types=1);
+
+namespace OrcaServices\Heartbeat\Test\TestCase\Heartbeat\Sensor;
+
+use Cake\Database\Connection;
+use Cake\Database\Driver\Sqlite;
+use Cake\Datasource\ConnectionManager;
+use Cake\TestSuite\TestCase;
+use OrcaServices\Heartbeat\Heartbeat\Sensor\Config;
+use OrcaServices\Heartbeat\Heartbeat\Sensor\DBConnection;
+
+/**
+ * DB Connection Sensor Test
+ *
+ * @coversDefaultClass DBConnection
+ */
+class DBConnectionTest extends TestCase
+{
+    /**
+     * Name of the test database connection.
+     *
+     * @var string
+     */
+    private const TEST_CONNECTION = 'heartbeat_db_connection_test';
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (!extension_loaded('pdo_sqlite')) {
+            $this->markTestSkipped('The pdo_sqlite extension is required for this test.');
+        }
+
+        ConnectionManager::setConfig(self::TEST_CONNECTION, [
+            'className' => Connection::class,
+            'driver' => Sqlite::class,
+            'database' => ':memory:',
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function tearDown(): void
+    {
+        ConnectionManager::drop(self::TEST_CONNECTION);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Builds a configured DBConnection sensor.
+     *
+     * @param array $settings Sensor settings
+     * @return DBConnection
+     */
+    private function createSensor(array $settings = []): DBConnection
+    {
+        $config = new Config('DB Connection', [
+            'enabled' => true,
+            'severity' => 3,
+            'class' => DBConnection::class,
+            'settings' => $settings,
+        ]);
+
+        return new DBConnection($config);
+    }
+
+    /**
+     * Test _getStatus for a healthy connection.
+     *
+     * @return void
+     * @covers ::_getStatus
+     */
+    public function testGetStatusReturnsTrueForReachableConnection(): void
+    {
+        $sensor = $this->createSensor(['connection_name' => self::TEST_CONNECTION]);
+
+        $status = $sensor->getStatus();
+
+        $this->assertTrue($status->getStatus());
+    }
+
+    /**
+     * Test _getStatus for an unknown connection.
+     *
+     * @return void
+     * @covers ::_getStatus
+     */
+    public function testGetStatusReturnsFalseForUnknownConnection(): void
+    {
+        $sensor = $this->createSensor(['connection_name' => 'this_connection_does_not_exist']);
+
+        $status = $sensor->getStatus();
+
+        $this->assertFalse($status->getStatus());
+    }
+}
