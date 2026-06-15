3b6cf4e79682ab378032db2f786ad3da4286eb89
Neluxx <fabian.arndt96@proton.me>
2026-06-15 10:26:47 +0200

    Add tests for DebugMode sensor

---
+++ tests/TestCase/Heartbeat/Sensor/DebugModeTest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace OrcaServices\Heartbeat\Test\TestCase\Heartbeat\Sensor;
+
+use Cake\Core\Configure;
+use Cake\TestSuite\TestCase;
+use OrcaServices\Heartbeat\Heartbeat\Sensor\Config;
+use OrcaServices\Heartbeat\Heartbeat\Sensor\DebugMode;
+
+/**
+ * Debug Mode Sensor Test
+ *
+ * @coversDefaultClass DebugMode
+ */
+class DebugModeTest extends TestCase
+{
+    /**
+     * Builds a configured DebugMode sensor.
+     *
+     * @return DebugMode
+     */
+    private function createSensor(): DebugMode
+    {
+        $config = new Config('Debug-Mode', [
+            'enabled' => true,
+            'severity' => 1,
+            'class' => DebugMode::class,
+        ]);
+
+        return new DebugMode($config);
+    }
+
+    /**
+     * When debug mode is enabled, the sensor reports the string '1'.
+     *
+     * @return void
+     * @covers ::_getStatus
+     */
+    public function testGetStatusReturnsOneWhenDebugIsEnabled(): void
+    {
+        Configure::write('debug', true);
+
+        $sensor = $this->createSensor();
+
+        $this->assertSame('1', $sensor->getStatus()->getStatus());
+    }
+
+    /**
+     * When debug mode is disabled, the sensor reports the string '' (the (string) cast of boolean false).
+     *
+     * @return void
+     * @covers ::_getStatus
+     */
+    public function testGetStatusReturnsEmptyStringWhenDebugIsDisabled(): void
+    {
+        Configure::write('debug', false);
+
+        $sensor = $this->createSensor();
+
+        $this->assertSame('', $sensor->getStatus()->getStatus());
+    }
+}
