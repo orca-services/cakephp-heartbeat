29e70a230aa0e4fc0f1ffbfb04d93dff9ade7731
Neluxx <fabian.arndt96@proton.me>
2026-06-17 07:31:59 +0200

    Replace PHPUnit TestCase with Cake TestCase

--- tests/TestCase/Heartbeat/Sensor/ConfigTest.php
+++ tests/TestCase/Heartbeat/Sensor/ConfigTest.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace OrcaServices\Heartbeat\Test\TestCase\Heartbeat\Sensor;
 
+use Cake\TestSuite\TestCase;
 use OrcaServices\Heartbeat\Heartbeat\Sensor\Config;
 use OrcaServices\Heartbeat\Test\TestCase\Sensor\DummySensor;
-use PHPUnit\Framework\TestCase;
 
 /**
  * Config Tests
