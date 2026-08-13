791c4ae63a90c562321cec73eb0d4474f3d80d12
marc.wuerth <marc.wuerth@orca.ch>
2026-08-13 14:42:06 +0200

    Assert message too

--- tests/TestCase/Heartbeat/Sensor/StatusTest.php
+++ tests/TestCase/Heartbeat/Sensor/StatusTest.php
@@ -31,21 +31,28 @@ class StatusTest extends TestCase
     {
         Chronos::setTestNow('2017-03-30 12:45:37');
 
+        $name = 'Dummy Sensor';
+        $statusBool = true;
+        $duration = 0;
+        $lastExecuted = Chronos::now();
+        $severity = Severity::INFORMATIONAL;
+        $message = __d('Heartbeat', 'OK');
+
         $status = new Status(
-            'Dummy Sensor',
-            true,
-            0,
-            Chronos::now(),
-            Severity::INFORMATIONAL,
-            __d('Heartbeat', 'OK'),
+            $name,
+            $statusBool,
+            $duration,
+            $lastExecuted,
+            $severity,
+            $message,
         );
 
-        $this->assertInstanceOf(Status::class, $status);
-        $this->assertEquals('Dummy Sensor', $status->name);
-        $this->assertEquals(true, $status->status);
-        $this->assertEquals(0, $status->duration);
-        $this->assertEquals('2017-03-30 12:45:37', $status->lastExecuted);
-        $this->assertEquals(Severity::INFORMATIONAL, $status->severity);
+        $this->assertEquals($name, $status->name);
+        $this->assertEquals($statusBool, $status->status);
+        $this->assertEquals($duration, $status->duration);
+        $this->assertEquals($lastExecuted->toDateTimeString(), $status->lastExecuted);
+        $this->assertEquals($severity, $status->severity);
+        $this->assertEquals($message, $status->message);
         $this->assertEquals(false, $status->wasCheckCached());
     }
 
